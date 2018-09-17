dd49e9b7860c66338469660b18cb998c218106e9
Travis Weir <tweirtx@gmail.com>
2018-09-17 13:37:57 -0500

    Stopped pissing off pylint

--- frcstreamer/__main__.py
+++ frcstreamer/__main__.py
@@ -1,32 +1,44 @@
+"""Returns FRC streams"""
+import datetime
 import tbapi
 import streamlink
-import datetime
 
 
 def get_raw_stream(url):
-	return streamlink.streams(url)
+    """Gets the raw streams"""
+    return streamlink.streams(url)
 
 
 def make_tba(api_key):
-	return tbapi.TBAParser(api_key)
+    """Makes a TBA parser"""
+    return tbapi.TBAParser(api_key)
 
 
 class FRCStreamer:
-	api_key = ""
+    """FRCStreamer"""
+    api_key = ""
 
-	tba = make_tba(api_key)
+    tba = make_tba(api_key)
 
-	def get_event_streams(self, event_key):
-		print(event_key)
-		return []
+    def get_event_streams(self, event_key):
+        """Get all streams for an event"""
+        print(event_key)
+        strems = []
+        event = self.tba.get_event(event_key)
+        for cast in event.webcasts:
+            strems.append(get_raw_stream(cast.url))
+        return strems
 
-	def get_stream_from_event(self, event_key):
-		return self.get_event_streams(event_key)[0]
+    def get_stream_from_event(self, event_key):
+        """Get 1 stream from an event"""
+        return self.get_event_streams(event_key)[0]
 
-	def get_all_streams(self):
-		print("Getting all streams")
-		list_of_events = [event for event in self.tba.get_event_list() if event.start_date <= datetime.datetime.now() <= event.end_date]
-		streams = []
-		for event in list_of_events:
-			streams.append({event.name: self.get_event_streams(event.key)})
-		return streams
+    def get_all_streams(self):
+        """Get every live FRC feed"""
+        print("Getting all streams")
+        list_of_events = [event for event in self.tba.get_event_list()
+                          if event.start_date <= datetime.datetime.now() <= event.end_date]
+        streams = []
+        for event in list_of_events:
+            streams.append({event.name: self.get_event_streams(event.key)})
+        return streams
